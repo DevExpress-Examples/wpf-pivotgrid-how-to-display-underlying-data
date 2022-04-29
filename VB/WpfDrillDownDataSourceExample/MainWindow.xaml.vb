8056ec4a4db11f53995c292d9492feb7bbfe827c
DevExpressExampleBot <fakeEmail>
2022-04-29 16:43:47 +0400

    Source auto update [skip ci]

--- VB/WpfDrillDownDataSourceExample/MainWindow.xaml.vb
+++ VB/WpfDrillDownDataSourceExample/MainWindow.xaml.vb
@@ -1,36 +1,41 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.PivotGrid
 Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace WpfDrillDownDataSourceExample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	''' 
 
-	Partial Public Class MainWindow
-		Inherits ThemedWindow
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    ''' 
+    Public Partial Class MainWindow
+        Inherits ThemedWindow
 
-		Public Property OrderSourceList() As ObservableCollection(Of MyOrderRow)
-		Public Property OrderDrillDownList() As ObservableCollection(Of MyOrderRow)
-		Public Sub New()
-			InitializeComponent()
-			OrderSourceList = DatabaseHelper.CreateData()
-			pivotGridControl1.DataSource = OrderSourceList
-		End Sub
-		Private Sub PivotGridControl1_CellSelectionChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			OrderDrillDownList = New ObservableCollection(Of MyOrderRow)()
-			For Each cellPoint As System.Drawing.Point In pivotGridControl1.MultiSelection.SelectedCells
-				For Each record As PivotDrillDownDataRow In pivotGridControl1.GetCellInfo(cellPoint.X, cellPoint.Y).CreateDrillDownDataSource()
-					OrderDrillDownList.Add(OrderSourceList(record.ListSourceRowIndex))
-				Next record
-			Next cellPoint
-			lvOrders.ItemsSource = OrderDrillDownList
-		End Sub
-		Private Sub ThemedWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGridControl1.BestFitArea = DevExpress.Xpf.PivotGrid.FieldBestFitArea.FieldHeader
-			pivotGridControl1.BestFit()
-		End Sub
-	End Class
+        Public Property OrderSourceList As ObservableCollection(Of MyOrderRow)
+
+        Public Property OrderDrillDownList As ObservableCollection(Of MyOrderRow)
+
+        Public Sub New()
+            Me.InitializeComponent()
+            OrderSourceList = CreateData()
+            Me.pivotGridControl1.DataSource = OrderSourceList
+        End Sub
+
+        Private Sub PivotGridControl1_CellSelectionChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            OrderDrillDownList = New ObservableCollection(Of MyOrderRow)()
+            For Each cellPoint As System.Drawing.Point In Me.pivotGridControl1.MultiSelection.SelectedCells
+                For Each record As PivotDrillDownDataRow In Me.pivotGridControl1.GetCellInfo(cellPoint.X, cellPoint.Y).CreateDrillDownDataSource()
+                    OrderDrillDownList.Add(OrderSourceList(record.ListSourceRowIndex))
+                Next
+            Next
+
+            Me.lvOrders.ItemsSource = OrderDrillDownList
+        End Sub
+
+        Private Sub ThemedWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGridControl1.BestFitArea = FieldBestFitArea.FieldHeader
+            Me.pivotGridControl1.BestFit()
+        End Sub
+    End Class
 End Namespace
--- VB/WpfDrillDownDataSourceExample/MyOrder.vb
+++ VB/WpfDrillDownDataSourceExample/MyOrder.vb
@@ -1,93 +1,105 @@
-﻿Imports System
+Imports System
 Imports System.Collections.ObjectModel
 
 Namespace WpfDrillDownDataSourceExample
-	Public Class MyOrderRow
-		Private privateID As Integer
-		Public Property ID() As Integer
-			Get
-				Return privateID
-			End Get
-			Friend Set(ByVal value As Integer)
-				privateID = value
-			End Set
-		End Property
-		Private privateOrderDate As DateTime
-		Public Property OrderDate() As DateTime
-			Get
-				Return privateOrderDate
-			End Get
-			Friend Set(ByVal value As DateTime)
-				privateOrderDate = value
-			End Set
-		End Property
-		Private privateQuantity As Integer
-		Public Property Quantity() As Integer
-			Get
-				Return privateQuantity
-			End Get
-			Friend Set(ByVal value As Integer)
-				privateQuantity = value
-			End Set
-		End Property
-		Private privateUnitPrice As Decimal
-		Public Property UnitPrice() As Decimal
-			Get
-				Return privateUnitPrice
-			End Get
-			Friend Set(ByVal value As Decimal)
-				privateUnitPrice = value
-			End Set
-		End Property
-		Private privateCustomerName As String
-		Public Property CustomerName() As String
-			Get
-				Return privateCustomerName
-			End Get
-			Friend Set(ByVal value As String)
-				privateCustomerName = value
-			End Set
-		End Property
-		Private privateProductName As String
-		Public Property ProductName() As String
-			Get
-				Return privateProductName
-			End Get
-			Friend Set(ByVal value As String)
-				privateProductName = value
-			End Set
-		End Property
-		Private privateCategoryName As String
-		Public Property CategoryName() As String
-			Get
-				Return privateCategoryName
-			End Get
-			Friend Set(ByVal value As String)
-				privateCategoryName = value
-			End Set
-		End Property
-		Private privateSalesPersonName As String
-		Public Property SalesPersonName() As String
-			Get
-				Return privateSalesPersonName
-			End Get
-			Friend Set(ByVal value As String)
-				privateSalesPersonName = value
-			End Set
-		End Property
-		Private privateExtendedPrice As Decimal
-		Public Property ExtendedPrice() As Decimal
-			Get
-				Return privateExtendedPrice
-			End Get
-			Friend Set(ByVal value As Decimal)
-				privateExtendedPrice = value
-			End Set
-		End Property
-	End Class
 
-	Public Class MyOrderDataList
-		Inherits ObservableCollection(Of MyOrderRow)
+    Public Class MyOrderRow
 
-	End Class
+        Private _ID As Integer, _OrderDate As DateTime, _Quantity As Integer, _UnitPrice As Decimal, _CustomerName As String, _ProductName As String, _CategoryName As String, _SalesPersonName As String, _ExtendedPrice As Decimal
+
+        Public Property ID As Integer
+            Get
+                Return _ID
+            End Get
+
+            Friend Set(ByVal value As Integer)
+                _ID = value
+            End Set
+        End Property
+
+        Public Property OrderDate As DateTime
+            Get
+                Return _OrderDate
+            End Get
+
+            Friend Set(ByVal value As DateTime)
+                _OrderDate = value
+            End Set
+        End Property
+
+        Public Property Quantity As Integer
+            Get
+                Return _Quantity
+            End Get
+
+            Friend Set(ByVal value As Integer)
+                _Quantity = value
+            End Set
+        End Property
+
+        Public Property UnitPrice As Decimal
+            Get
+                Return _UnitPrice
+            End Get
+
+            Friend Set(ByVal value As Decimal)
+                _UnitPrice = value
+            End Set
+        End Property
+
+        Public Property CustomerName As String
+            Get
+                Return _CustomerName
+            End Get
+
+            Friend Set(ByVal value As String)
+                _CustomerName = value
+            End Set
+        End Property
+
+        Public Property ProductName As String
+            Get
+                Return _ProductName
+            End Get
+
+            Friend Set(ByVal value As String)
+                _ProductName = value
+            End Set
+        End Property
+
+        Public Property CategoryName As String
+            Get
+                Return _CategoryName
+            End Get
+
+            Friend Set(ByVal value As String)
+                _CategoryName = value
+            End Set
+        End Property
+
+        Public Property SalesPersonName As String
+            Get
+                Return _SalesPersonName
+            End Get
+
+            Friend Set(ByVal value As String)
+                _SalesPersonName = value
+            End Set
+        End Property
+
+        Public Property ExtendedPrice As Decimal
+            Get
+                Return _ExtendedPrice
+            End Get
+
+            Friend Set(ByVal value As Decimal)
+                _ExtendedPrice = value
+            End Set
+        End Property
+    End Class
+
+    Public Class MyOrderDataList
+        Inherits System.Collections.ObjectModel.ObservableCollection(Of WpfDrillDownDataSourceExample.MyOrderRow)
+
+    End Class
 End Namespace
